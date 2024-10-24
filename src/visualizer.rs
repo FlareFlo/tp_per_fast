c3f8bf349cb8a485d4baf40dc1baaf787879a5aa
FlareFlo <mail@flareflo.dev>
2024-10-24 14:06:42 +0200

    Improve color palette

--- src/visualizer.rs
+++ src/visualizer.rs
@@ -2,8 +2,10 @@ use std::fs;
 
 use geo::{Coord, CoordsIter, Geometry, MultiPolygon};
 use image::{ImageBuffer, Rgb};
-use image::imageops::flip_vertical;
-use imageproc::drawing::draw_line_segment_mut;
+use image::imageops::{flip_vertical, interpolate_bilinear};
+use imageproc::drawing::{draw_antialiased_line_segment_mut, draw_line_segment_mut, Blend};
+use imageproc::pixelops::interpolate;
+use petgraph::visit::Walker;
 use prost::Message;
 use wkt::TryFromWkt;
 
@@ -48,20 +50,21 @@ fn main() {
 	let img_width = (((min_lon / max_lon) * img_height as f64)) as u32;
 	//let img_width = img_height;
 	let mut img = ImageBuffer::from_fn(img_width, img_height, |_x, _y| {
-		Rgb([255, 255, 255]) // White background
+		Rgb([28, 28, 28]) // White background
 	});
 
 	// Load the GeoJSON file and extract the features
 	for bezirk in parsed {
-		draw_polygons(&mut img, bezirk.location.try_into().unwrap());
+		draw_polygons(&mut img, bezirk);
 	}
 	img = flip_vertical(&img);
 	// Save the image
 	img.save("map.png").unwrap();
 }
 
-fn draw_polygons(image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, geodata: MultiPolygon) {
-	for polygon in geodata.iter() {
+fn draw_polygons(image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, bezirk: Bezirk) {
+	let geo: &MultiPolygon= &bezirk.location.try_into().unwrap();
+	for polygon in geo.iter() {
 		for arr in polygon.exterior().lines().collect::<Vec<_>>().windows(2) {
 			let old = arr[1];
 			let new = arr[0];
@@ -71,14 +74,15 @@ fn draw_polygons(image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, geodata: MultiPolygo
 
 			let start = (y1, x1);
 			let end = (y2, x2);
-			let color = Rgb([0, 0, 0]);
-			draw_line_segment_mut(image, start, end, color);
+			let colrs = [Rgb([255,255,255]), Rgb([255, 87, 20]), Rgb([232, 170, 20]), Rgb([228, 255, 26]), Rgb([110, 235, 131]), Rgb([27, 231, 255]), Rgb([255,27,117]), Rgb([255,51,27])];
+			let color = colrs[bezirk.parents.len()];
+			draw_antialiased_line_segment_mut(image, start, end, color, interpolate);
 		}
 	}
 }
 
-fn scale_coordinates(lat: f64, lon: f64, img_width: u32, img_height: u32) -> (f32, f32) {
+fn scale_coordinates(lat: f64, lon: f64, img_width: u32, img_height: u32) -> (i32, i32) {
 	let x = (((lon - min_lon) / (max_lon - min_lon)).abs() * img_height as f64);
 	let y = (((lat - min_lat) / (max_lat - min_lat)).abs() * img_width as f64);
-	(x as f32, y as f32)
+	(x as i32, y as i32)
 }
