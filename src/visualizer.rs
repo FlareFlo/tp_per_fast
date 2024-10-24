8af93550347e5f102ce45cbd17f97fffef1e2234
FlareFlo <mail@flareflo.dev>
2024-10-24 14:17:23 +0200

    SVG WIP

--- src/visualizer.rs
+++ src/visualizer.rs
@@ -1,3 +1,4 @@
+use svg::node::element::{Group, Polygon};
 use std::fs;
 
 use geo::{Coord, CoordsIter, Geometry, MultiPolygon};
@@ -7,6 +8,8 @@ use imageproc::drawing::{draw_antialiased_line_segment_mut, draw_line_segment_mu
 use imageproc::pixelops::interpolate;
 use petgraph::visit::Walker;
 use prost::Message;
+use svg::Document;
+use svg::node::element::Path;
 use wkt::TryFromWkt;
 
 #[derive(Clone, Debug)]
@@ -46,43 +49,27 @@ fn main() {
 			location:   Geometry::try_from_wkt_str(&e.wkt).unwrap(),
 		})
 		.collect();
-	let img_height = 8000;
-	let img_width = (((min_lon / max_lon) * img_height as f64)) as u32;
-	//let img_width = img_height;
-	let mut img = ImageBuffer::from_fn(img_width, img_height, |_x, _y| {
-		Rgb([28, 28, 28]) // White background
-	});
+	let mut document = Document::new().set("viewBox", "0 0 100 100");
 
 	// Load the GeoJSON file and extract the features
 	for bezirk in parsed {
-		draw_polygons(&mut img, bezirk);
+		document = draw_polygons(document, bezirk);
 	}
-	img = flip_vertical(&img);
-	// Save the image
-	img.save("map.png").unwrap();
+	    svg::save("multipolygon.svg", &document).expect("Failed to save SVG file");
 }
 
-fn draw_polygons(image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, bezirk: Bezirk) {
+fn draw_polygons(mut document: Document, bezirk: Bezirk) -> Document {
 	let geo: &MultiPolygon= &bezirk.location.try_into().unwrap();
-	for polygon in geo.iter() {
-		for arr in polygon.exterior().lines().collect::<Vec<_>>().windows(2) {
-			let old = arr[1];
-			let new = arr[0];
-			let (x1, y1) = scale_coordinates(old.end.x, old.end.y, image.width(), image.height());
-			let (x2, y2) =
-				scale_coordinates(new.start.x, new.start.y, image.width(), image.height());
+	    for polygon in geo.iter() {
+        let svg_polygon = Polygon::new()
+            .set("fill", "blue")
+            .set("stroke", "black")
+            .set("stroke-width", 1)
+            .set("points", polygon.exterior().points().map(|p| {
+                format!("{},{}", p.x(), p.y())
+            }).collect::<Vec<String>>().join(" "));
 
-			let start = (y1, x1);
-			let end = (y2, x2);
-			let colrs = [Rgb([255,255,255]), Rgb([255, 87, 20]), Rgb([232, 170, 20]), Rgb([228, 255, 26]), Rgb([110, 235, 131]), Rgb([27, 231, 255]), Rgb([255,27,117]), Rgb([255,51,27])];
-			let color = colrs[bezirk.parents.len()];
-			draw_antialiased_line_segment_mut(image, start, end, color, interpolate);
-		}
-	}
-}
-
-fn scale_coordinates(lat: f64, lon: f64, img_width: u32, img_height: u32) -> (i32, i32) {
-	let x = (((lon - min_lon) / (max_lon - min_lon)).abs() * img_height as f64);
-	let y = (((lat - min_lat) / (max_lat - min_lat)).abs() * img_width as f64);
-	(x as i32, y as i32)
+        document = document.add(Group::new().add(svg_polygon));
+    }
+	document
 }
