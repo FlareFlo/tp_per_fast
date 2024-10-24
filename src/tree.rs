9de53935cf385604900f47e2cbb912cf8c4efc03
FlareFlo <mail@flareflo.dev>
2024-10-24 10:52:54 +0200

    Add treeviz

---
+++ src/tree.rs
@@ -0,0 +1,44 @@
+use std::fs;
+use petgraph::dot::{Config, Dot};
+use petgraph::graph::DiGraph;
+use prost::Message;
+use crate::protobufs::Bezirk;
+
+pub mod protobufs {
+	pub use Bezirk as ProtobufBezirk;
+	include!(concat!(env!("OUT_DIR"), "/geodata.rs"));
+	include!(concat!(env!("OUT_DIR"), "/wire.rs"));
+}
+
+pub fn main() {
+	let bezirke = protobufs::File::decode(
+		fs::read("/home/flareflo/tp_per/group-b/geodata/result/geodata/bezirke-12.geodata")
+			.unwrap()
+			.as_slice(),
+	)
+	.unwrap();
+
+	let mut graph = DiGraph::<_, ()>::new();
+
+	let mut node_map = std::collections::HashMap::new();
+
+    // Add nodes to the graph
+    for node in &bezirke.bezirke {
+        let idx = graph.add_node(&node.name);
+        node_map.insert(node.identifier, idx);
+    }
+
+    for node in &bezirke.bezirke {
+        let node_idx = node_map[&node.identifier];
+
+        // Add an edge for every parent in the `parents` path
+        if let Some(&last_parent) = node.parents.get(0) {
+            let parent_idx = node_map[&last_parent];
+            graph.add_edge(parent_idx, node_idx, ());
+        }
+    }
+
+	println!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
+}
+// cargo r --bin tree > out.dot
+// dot -Granksep=4 -Tsvg out.dot -o tree.svg
